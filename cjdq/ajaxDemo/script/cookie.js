5edec8cad3e783a25b5317023725de296fb934d3
WuChenDi <996194720@qq.com>
2018-06-20 10:40:11 +0800

    cjdq -- 2017/5-2018/3

---
+++ cjdq/ajaxDemo/script/cookie.js
@@ -0,0 +1,25 @@
+﻿var cookie = {
+    get: function (name) {
+        var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+        if (arr = document.cookie.match(reg))
+            return unescape(arr[2]);
+        else
+            return null;
+    },
+    set: function (name, value, time) {
+        var exp = new Date();
+
+        if (time == undefined)
+            time = 30 * 24 * 60 * 60 * 1000;
+
+        exp.setTime(exp.getTime() + time);
+        document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+    },
+    del: function (name) {
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval = getCookie(name);
+        if (cval != null)
+            document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+    }
+}
