93c171133a6047e74d5c9c3e3ecd1046b78b9ee5
WuChenDi <996194720@qq.com>
2019-06-25 20:34:31 +0800

    update loading

--- 07-nodejs/blog-1/src/router/user.js
+++ 07-nodejs/blog-1/src/router/user.js
@@ -5,11 +5,24 @@ const handleUserRouter = (req, res) => {
     const method = req.method // GET POST
 
     // 登录
-    if (method === "POST" && req.path === "/api/user/login") {
-        const { username, password } = req.body
+    // if (method === "POST" && req.path === "/api/user/login") {
+    //     const { username, password } = req.body
+    //     const result = login(username, password)
+    //     return result.then(data => {
+    //         if (data.username) {
+    //             return new SuccessModel()
+    //         }
+    //         return new ErrorModel('登录失败')
+    //     })
+    // }
+    if (method === "GET" && req.path === "/api/user/login") {
+        const { username, password } = req.query
         const result = login(username, password)
         return result.then(data => {
             if (data.username) {
+                console.log(data)
+                // 操作cookie
+                res.setHeader('Set-Cookie', `username='${data.username}';path=/`)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')
