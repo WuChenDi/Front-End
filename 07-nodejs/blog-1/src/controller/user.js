6c1be4642e41e6ef30383718c1bdf047d6abf802
WuChenDi <996194720@qq.com>
2019-09-02 21:21:24 +0800

    防止sql注入 escape

--- 07-nodejs/blog-1/src/controller/user.js
+++ 07-nodejs/blog-1/src/controller/user.js
@@ -1,19 +1,21 @@
-const { exec } = require('../db/mysql')
-
+const { exec, escape } = require("../db/mysql");
 
 const login = (username, password) => {
     // if (username === "wcd" && password === "123") {
     //     return true
     // }
     // return false
+    username = escape(username);
+    password = escape(password);
     const sql = `
-        select username, realname from users where username='${username}' and password='${password}'
-    `
+        select username, realname from users where username=${username} and password=${password}
+    `;
+    console.log("sql is" + sql);
     return exec(sql).then(rows => {
-        return rows[0] || {}
-    })
-}
+        return rows[0] || {};
+    });
+};
 
 module.exports = {
     login
-}
+};
