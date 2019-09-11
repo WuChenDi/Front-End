04798b350180c81f8b004659f6e4ddb9b3d555c2
WuChenDi <996194720@qq.com>
2019-09-11 21:35:39 +0800

    update n

---
+++ 07-nodejs/blog-express/middleware/loginCheck.js
@@ -0,0 +1,7 @@
+const { SuccessModel, ErrorModel } = require("../model/resModel");
+
+module.exports = (req, res, next) => {
+    if (req, session.username) {
+        
+    }
+}
--- 07-nodejs/blog-express/routes/blog.js
+++ 07-nodejs/blog-express/routes/blog.js
@@ -10,20 +10,20 @@ const {
 const { SuccessModel, ErrorModel } = require("../model/resModel");
 
 /* GET home page. */
-router.get("/list", function(req, res, next) {
+router.get("/list", function (req, res, next) {
     let author = req.query.author || "";
     const keyword = req.query.keyword || "";
 
-    // if (req.query.isadmin) {
-    //     // 管理员界面
-    //     const loginCheckResult = loginCheck(req);
-    //     if (loginCheckResult) {
-    //         // 未登录
-    //         return loginCheckResult;
-    //     }
-    //     // 强制查询自己的博客
-    //     author = req.session.username;
-    // }
+    if (req.query.isadmin) {
+        // // 管理员界面
+        // const loginCheckResult = loginCheck(req);
+        // if (loginCheckResult) {
+        //     // 未登录
+        //     return loginCheckResult;
+        // }
+        // // 强制查询自己的博客
+        author = req.session.username;
+    }
 
     const result = getList(author, keyword);
     return result.then(listData => {
@@ -31,7 +31,7 @@ router.get("/list", function(req, res, next) {
     });
 });
 
-router.get("/detail", function(req, res, next) {
+router.get("/detail", function (req, res, next) {
     res.json({
         error: 0,
         data: "OK"
