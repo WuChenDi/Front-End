2d61ebf90dcce34ca93d73b78bd32335089b4062
WuChenDi <996194720@qq.com>
2019-09-11 09:02:28 +0800

    update express

--- 07-nodejs/blog-express/app.js
+++ 07-nodejs/blog-express/app.js
@@ -4,25 +4,25 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+// var indexRouter = require("./routes/index");
+// var usersRouter = require("./routes/users");
 const blogRouter = require("./routes/blog");
 const userRouter = require("./routes/user");
 
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/user", userRouter);
 
--- 07-nodejs/blog-express/routes/blog.js
+++ 07-nodejs/blog-express/routes/blog.js
@@ -1,11 +1,33 @@
 var express = require("express");
 var router = express.Router();
+const {
+    getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require("../controller/blog");
+const { SuccessModel, ErrorModel } = require("../model/resModel");
 
 /* GET home page. */
 router.get("/list", function(req, res, next) {
-    res.json({
-        error: 0,
-        data: [1, 2, 3]
+    let author = req.query.author || "";
+    const keyword = req.query.keyword || "";
+
+    // if (req.query.isadmin) {
+    //     // 管理员界面
+    //     const loginCheckResult = loginCheck(req);
+    //     if (loginCheckResult) {
+    //         // 未登录
+    //         return loginCheckResult;
+    //     }
+    //     // 强制查询自己的博客
+    //     author = req.session.username;
+    // }
+
+    const result = getList(author, keyword);
+    return result.then(listData => {
+        res.json(new SuccessModel(listData));
     });
 });
 
