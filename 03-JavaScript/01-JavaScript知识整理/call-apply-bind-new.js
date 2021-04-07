0c2a7ca2b380acefac6b88b35d224247aaf6b30f
WuChenDi <996194720@qq.com>
2021-04-07 10:27:00 +0800

    fix: 添加 this 方法判断

--- 03-JavaScript/01-JavaScript知识整理/call-apply-bind-new.js
+++ 03-JavaScript/01-JavaScript知识整理/call-apply-bind-new.js
@@ -1,5 +1,8 @@
 // call
 Function.prototype.Dcall = function (context) {
+	if (typeof this !== "function") {
+		throw new TypeError("Error");
+	}
 	var context = context || window;
 	context.say = this;
 	var args = [];
@@ -13,6 +16,9 @@ Function.prototype.Dcall = function (context) {
 
 // apply
 Function.prototype.Dapply = function (context, arr) {
+	if (typeof this !== "function") {
+		throw new TypeError("Error");
+	}
 	var context = context || window;
 	var result;
 	context.fn = this;
@@ -30,6 +36,9 @@ Function.prototype.Dapply = function (context, arr) {
 
 // bind
 Function.prototype.Dbind = function (context) {
+	if (typeof this !== "function") {
+		throw new TypeError("Error");
+	}
 	var self = this;
 	var args = Array.prototype.slice.call(arguments, 1);
 	return function () {
@@ -46,10 +55,10 @@ Function.prototype.Nnew = function (fn, ...args) {
 };
 
 // 或
-// Function.prototype.Dnew = function () {
-// 	var obj = {};
-// 	var Constructor = Array.prototype.shift.call(arguments);
-// 	obj.__proto__ = Constructor.prototype;
-// 	var result = Constructor.call(obj, arguments);
-// 	return result instanceof Object ? result : obj;
-// };
+Function.prototype.DDnew = function () {
+	var obj = {};
+	var Constructor = Array.prototype.shift.call(arguments);
+	obj.__proto__ = Constructor.prototype;
+	var result = Constructor.call(obj, arguments);
+	return result instanceof Object ? result : obj;
+};
