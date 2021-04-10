f4f6a47f7a95a782beb3ccd8c43342a9092e41e6
WuChenDi <996194720@qq.com>
2021-04-10 15:14:39 +0800

    feat: learing node mysql

---
+++ 07-nodejs/myblog.sql
@@ -0,0 +1,54 @@
+SELECT VERSION();
+
+CREATE SCHEMA `myblog123`;
+
+CREATE TABLE `myblog`.`users` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(20) NOT NULL,
+	`password` VARCHAR(20) NOT NULL,
+	`realname` VARCHAR(10) NOT NULL,
+	PRIMARY KEY (`id`));
+	
+CREATE TABLE `myblog`.`blogs` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(50) NOT NULL,
+	`content` LONGTEXT NOT NULL,
+	`createtime` BIGINT(20) NOT NULL DEFAULT 0,
+	`author` VARCHAR(20) NOT NULL,
+	PRIMARY KEY (`id`));
+	
+use myblog
+
+SHOW TABLES;
+
+INSERT INTO users(username, `password`, realname) VALUES('ww', '123', 'wuwu');
+INSERT INTO users(username, `password`, realname) VALUES('cc', '123', 'chenchen');
+INSERT INTO users(username, `password`, realname) VALUES('dd', '123', 'didi');
+INSERT INTO users(username, `password`, realname) VALUES('test', '123', 'testName');
+
+
+SELECT * FROM users;
+SELECT id, username FROM users;
+SELECT * FROM users WHERE state=1;
+SELECT * FROM users WHERE username='dd';
+SELECT * FROM users WHERE username='dd' and `password`='123';
+SELECT * FROM users WHERE username='dd' or `password`='123';
+SELECT * FROM users WHERE username LIKE '%d%';
+SELECT * FROM users ORDER BY id;
+SELECT * FROM users ORDER BY id DESC;
+
+
+UPDATE users set realname='didi2' WHERE username='dd';
+UPDATE users set state=0 WHERE username='test';
+
+DELETE FROM users WHERE username='dd3';
+DELETE FROM users WHERE id=6;
+
+
+INSERT INTO blogs(title, content, createtime, author) VALUES ('标题1', '内容1', '1618037480417', 'dd');
+
+SELECT * FROM blogs ORDER BY createtime DESC;
+
+SELECT * FROM blogs WHERE author='dd' ORDER BY createtime DESC;
+SELECT * FROM blogs WHERE title LIKE '%标题%' ORDER BY createtime DESC;
+
--- 07-nodejs/mysql-test/index.js
+++ 07-nodejs/mysql-test/index.js
@@ -1,31 +1,32 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 // 创建链接对象
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'wuchendi',
-    port: '3306',
-    database: 'myblog'
-})
+	host: "localhost",
+	user: "root",
+	password: "wcd0530",
+	port: "3306",
+	database: "myblog",
+});
 
 // 开始连接
-con.connect()
+con.connect();
 
 // 执行 sql 语句
-// const sql = 'select * from users'
-// const sql = 'select id, username from users'
-// const sql = `SELECT * FROM users WHERE state='1'`
-// const sql = `UPDATE users set state='0' WHERE username='wcd'`
+// const sql = "select * from users";
+const sql = "select id, username from users";
+// const sql = `SELECT * FROM users WHERE state='1'`;
+// const sql = `UPDATE users set state='0' WHERE username='wcd'`;
 // const sql = `update users set realname='wuchendi3' where username = 'wcd1'`;
-const sql = `insert into blogs (title, content, createtime, author) values ('标题', '内容',1560818599043, 'wuchendi')`
+// const sql = `insert into blogs (title, content, createtime, author) values ('标题', '内容',1560818599043, 'wuchendi')`;
 con.query(sql, (err, result) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-    console.log(result)
-})
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log(result);
+	// console.log(JSON.stringify(result));
+});
 
 // 关闭连接
-con.end()
+con.end();
