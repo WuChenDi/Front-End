d3b1f5ed7188e718cf4f252618edb6929ce44dea
WuChenDi <996194720@qq.com>
2021-04-24 22:28:36 +0800

    feat: node protogenesis

--- 07-nodejs/blog-1/bin/www.js
+++
@@ -1,7 +0,0 @@
-const http = require('http')
-
-const PORT = 8000
-const serverHandle = require('../app')
-
-const server = http.createServer(serverHandle)
-server.listen(PORT)
--- 07-nodejs/blog/src/controller/user.js
+++ 07-nodejs/blog/src/controller/user.js
@@ -1,10 +1,22 @@
-const loginCheck = (username, password) => {
-    if (username === "wcd" && password === "123") {
-        return true
-    }
-    return false
-}
+const { exec, escape } = require("../db/mysql");
+const { genPassword } = require("../utils/cryp");
+
+const login = (username, password) => {
+	username = escape(username);
+
+	// 生成加密密码
+	password = genPassword(password);
+	password = escape(password);
+
+	const sql = `
+        select username, realname from users where username=${username} and password=${password}
+    `;
+	console.log('sql is', sql)
+	return exec(sql).then((rows) => {
+		return rows[0] || {};
+	});
+};
 
 module.exports = {
-    loginCheck
-}
+	login,
+};
