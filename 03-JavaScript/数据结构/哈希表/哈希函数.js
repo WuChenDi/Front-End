9d9ff68c15d1d9e634b07f23d061cd3fef64d0ec
WuChenDi <996194720@qq.com>
2021-05-13 17:16:06 +0800

    refactor: 优化代码-数据结构-哈希表

--- 03-JavaScript/数据结构/哈希表/哈希函数.js
+++ 03-JavaScript/数据结构/哈希表/哈希函数.js
@@ -1,24 +1,29 @@
 /**
  * 设计哈希函数
- * 1.将字符串转成比较大的数字：hasCode
- * 2.将大的数字hasCode压缩到数组范围（大小）之内
+ * 1. 将字符串转换成比较大的数字，这个数字称为 hashCode
+ * 2. 将大的数字压缩到数组(size) 范围之内既 index
  *
+ * 公式：
+ * hashCode = hashCode * PRIME_NUMBER + key.charCodeAt(i);
+ * 取余操作：hashCode % size;(霍纳法则)
+ * PRIME_NUMBER 为质数，且小于数组的容量
+ * size为哈希表的长度
  */
 
-function hasFunc(str, size) {
-	//定义hasCode变量
-	let hasCode = 0;
-	//霍纳算法，来计算hasCode的值
-	// Unicode编码
-	for (let i = 0; i < str.length; i++) {
-		hasCode = 37 * hasCode + str.charCodeAt(i);
-	}
-	// 取余操作
-	let index = hasCode % size;
-	return index;
+function hasFunc(str, size = 7) {
+  // 质数
+  const PRIME_NUMBER = 37;
+  // 定义hasCode变量
+  let hasCode = 0;
+  // 霍纳算法，来计算hasCode的值
+  for (let i = 0; i < str.length; i++) {
+    hasCode = PRIME_NUMBER * hasCode + str.charCodeAt(i);
+  }
+  // 取余操作
+  return hasCode % size;
 }
 
-console.log(hasFunc("dsvdsvdfvfdv", 7));
-console.log(hasFunc("Abcds", 7));
-console.log(hasFunc("hytknmtp", 7));
-console.log(hasFunc("emllosdfh", 7));
+console.log(hasFunc("aa", 7));
+console.log(hasFunc("AA", 7));
+console.log(hasFunc("bb", 7));
+console.log(hasFunc("BB", 7));
--- 03-JavaScript/数据结构/哈希表/哈希表.js
+++ 03-JavaScript/数据结构/哈希表/哈希表.js
@@ -1,104 +1,100 @@
-/**
-
- */
-
 class HasTable {
-	constructor() {
-		this.storage = [];
-		this.count = 0;
-		this.limit = 7;
-	}
+  constructor() {
+    this.storage = [];
+    this.count = 0;
+    this.limit = 7;
+  }
 
-	//哈希函数
-	hasFunc(str, size) {
-		//定义hasCode变量
-		let hasCode = 0;
-		//霍纳算法，来计算hasCode的值
-		// Unicode编码
-		for (let i = 0; i < str.length; i++) {
-			hasCode = 37 * hasCode + str.charCodeAt(i);
-		}
-		// 取余操作
-		let index = hasCode % size;
-		return index;
-	}
+  //哈希函数
+  hasFunc(str, size = 7) {
+    // 质数
+    const PRIME_NUMBER = 37;
+    // 定义 hasCode 变量
+    let hasCode = 0;
+    // 霍纳算法，来计算hasCode的值
+    for (let i = 0; i < str.length; i++) {
+      hasCode = PRIME_NUMBER * hasCode + str.charCodeAt(i);
+    }
+    // 取余操作
+    return hasCode % size;
+  }
 
-	//插入修改操作
-	put(key, value) {
-		//根据Key获取index
-		let index = this.hasFunc(key, this.limit);
-		//根据index取出对应的bucket
-		let bucket = this.storage[index];
-		//判断buckey是否为null
-		if (bucket == null) {
-			bucket = [];
-			this.storage[index] = bucket;
-		}
-		//判断是否修改数据
-		for (let i = 0; i < bucket.length; i++) {
-			let tuple = bucket[i];
-			if (tuple[0] === key) {
-				tuple[1] = value;
-				return;
-			}
-		}
-		//添加操作
-		bucket.push([key, value]);
-		this.count++;
-	}
-	//获取操作
-	get(key) {
-		//根据key获取index
-		let index = this.hasFunc(key, this.limit);
-		//根据index获取对应的bucket
-		let bucket = this.storage[index];
-		//判断bucket是否空
-		if (bucket === null) {
-			return null;
-		}
-		//有bucket那么进行线性查找
-		for (let i = 0; i < bucket.length; i++) {
-			let tuple = bucket[i];
-			if (tuple[0] === key) {
-				return tuple[1];
-			}
-		}
-		//没有找到，那么返回Null
-		return null;
-	}
+  //插入修改操作
+  put(key, value) {
+    //根据Key获取index
+    let index = this.hasFunc(key, this.limit);
+    //根据index取出对应的bucket
+    let bucket = this.storage[index];
+    //判断buckey是否为null
+    if (bucket == null) {
+      bucket = [];
+      this.storage[index] = bucket;
+    }
+    //判断是否修改数据
+    for (let i = 0; i < bucket.length; i++) {
+      let tuple = bucket[i];
+      if (tuple[0] === key) {
+        tuple[1] = value;
+        return;
+      }
+    }
+    //添加操作
+    bucket.push([key, value]);
+    this.count++;
+  }
+  //获取操作
+  get(key) {
+    //根据key获取index
+    let index = this.hasFunc(key, this.limit);
+    //根据index获取对应的bucket
+    let bucket = this.storage[index];
+    //判断bucket是否空
+    if (bucket === null) {
+      return null;
+    }
+    //有bucket那么进行线性查找
+    for (let i = 0; i < bucket.length; i++) {
+      let tuple = bucket[i];
+      if (tuple[0] === key) {
+        return tuple[1];
+      }
+    }
+    //没有找到，那么返回Null
+    return null;
+  }
 
-	//删除操作
-	remove(key) {
-		//根据key获取index
-		let index = this.hasFunc(key, this.limit);
-		//根据index获取对应的bucket
-		let bucket = this.storage[index];
-		//判断bucket是否空
-		if (bucket === null) {
-			return null;
-		}
-		//有bucket那么进行线性查找,并且删除
-		for (let i = 0; i < bucket.length; i++) {
-			let tuple = bucket[i];
-			if (tuple[0] === key) {
-				bucket.splice(i, 1);
-				this.count--;
-				return tuple[1];
-			}
-		}
-		//没有找到，那么返回Null
-		return null;
-	}
+  //删除操作
+  remove(key) {
+    //根据key获取index
+    let index = this.hasFunc(key, this.limit);
+    //根据index获取对应的bucket
+    let bucket = this.storage[index];
+    //判断bucket是否空
+    if (bucket === null) {
+      return null;
+    }
+    //有bucket那么进行线性查找,并且删除
+    for (let i = 0; i < bucket.length; i++) {
+      let tuple = bucket[i];
+      if (tuple[0] === key) {
+        bucket.splice(i, 1);
+        this.count--;
+        return tuple[1];
+      }
+    }
+    //没有找到，那么返回Null
+    return null;
+  }
 
-	//判断哈希表是否为空
-	isEmpty() {
-		return this.count === 0;
-	}
+  //判断哈希表是否为空
+  isEmpty() {
+    return this.count === 0;
+  }
 
-	//获取哈希表元素个数
-	size() {
-		return this.count;
-	}
+  //获取哈希表元素个数
+  size() {
+    return this.count;
+  }
 }
 
 const hasTable = new HasTable();
