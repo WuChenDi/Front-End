d0e07a6df2eb4753a603c27418577a82d8453d3f
WuChenDi <996194720@qq.com>
2021-05-21 10:29:39 +0800

    feat: sentry 项目接入测试

--- 10-微前端/qiankun/qiankun-react/src/index.js
+++ 10-微前端/qiankun/qiankun-react/src/index.js
@@ -1,31 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import * as Sentry from "@sentry/react";
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
 
-Sentry.init({ dsn: "http://62930970a87740cf8829d88cde4d0fc3@10.100.68.135:9000/2" });
+Sentry.init({
+  dsn: "http://62930970a87740cf8829d88cde4d0fc3@10.100.68.163:9000/2",
+  integrations: [new Integrations.BrowserTracing()],
+  // 添加用户崩溃反馈弹窗
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id });
+    }
+    return event;
+  },
+});
 
 function render() {
-	ReactDOM.render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-		document.getElementById("root")
-	);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 }
 
 if (!window.__POWERED_BY_QIANKUN__) {
-	render();
+  render();
 }
 
 export async function bootstrap() {}
 
 export async function mount(props) {
-	render();
+  render();
 }
 
 export async function unmount(props) {
-	ReactDOM.unmountComponentAtNode(document.getElementById("root"));
+  ReactDOM.unmountComponentAtNode(document.getElementById("root"));
 }
