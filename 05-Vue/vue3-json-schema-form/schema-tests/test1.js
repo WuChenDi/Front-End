57578df3c210108c76cfc4b4fc72a9a3a4f09112
WuChenDi <996194720@qq.com>
2021-05-29 10:29:06 +0800

    fix: add ajv-i18n ajv-errors

--- 05-Vue/vue3-json-schema-form/schema-tests/test1.js
+++ 05-Vue/vue3-json-schema-form/schema-tests/test1.js
@@ -1,13 +1,20 @@
 const Ajv = require('ajv');
+const localize = require("ajv-i18n")
 
 const schema = {
   type: 'object',
   properties: {
     name: {
       type: 'string',
+      // test: false,
+      // errorMessage: '这是不对的',
+      errorMessage: {
+        type: '必须是字符串',
+        minLength: '长度不能小于10'
+      },
       // format: 'email'
-      format: 'test'
-      // minLength: 10
+      // format: 'test'
+      minLength: 10
     },
     age: {
       type: 'number'
@@ -25,10 +32,34 @@ const schema = {
   required: ['name', 'age']
 }
 
-const ajv = new Ajv();
-ajv.addFormat('test', (data) => {
-  console.log(data, '--------');
-  return data === 'wcd'
+const ajv = new Ajv({allErrors: true});
+require('ajv-errors')(ajv)
+// ajv.addFormat('test', (data) => {
+//   console.log(data, '--------');
+//   return data === 'wcd'
+// })
+ajv.addKeyword('test', {
+  // validate(schema, data) {
+  //   console.log(schema, data)
+  //   if (schema === true) {
+  //     return true
+  //   } else {
+  //     return schema.length === 6
+  //   }
+  // }
+  // compile(sch, parentSchema) {
+  //   console.log(sch, parentSchema)
+  //   // return true
+  //   return () => true
+  // },
+  // metaSchema: {
+  //   type: 'boolean'
+  // }
+  macro() {
+    return {
+      minLength: 10
+    }
+  }
 })
 const validate = ajv.compile(schema);
 const valid = validate({
@@ -38,4 +69,25 @@ const valid = validate({
   pets: ['mimi', 'mama'],
   isWorker: true
 });
-if (!valid) console.log(validate.errors)
+if (!valid) {
+  localize.zh(validate.errors)
+  console.log(validate.errors)
+}
+
+
+// const Ajv = require("ajv").default
+// const ajv = new Ajv({allErrors: true})
+// require("ajv-errors")(ajv)
+// const schema = {
+//   type: "object",
+//   required: ["foo"],
+//   properties: {
+//     foo: {type: "integer"},
+//   },
+//   additionalProperties: false,
+//   errorMessage: "should be an object with an integer property foo only",
+// }
+//
+// const validate = ajv.compile(schema)
+// console.log(validate({foo: "a", bar: 2}))
+// console.log(validate.errors)
