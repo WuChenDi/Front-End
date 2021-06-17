aee712e47f25cfe40a84aebb2fe8b1c4c6e88d8e
WuChenDi <996194720@qq.com>
2021-06-17 15:15:50 +0800

    fix: 注释 modules 自动引入，会导致 ts 类型推断报错

--- 05-Vue/vite-vue-ts/src/store/modules.ts
+++ 05-Vue/vite-vue-ts/src/store/modules.ts
@@ -1,15 +1,23 @@
-const asyncStoreModule = async () => {
-  const storeModules = import.meta.globEager("./modules/**/index.ts");
+// const asyncStoreModule = async () => {
+//   const storeModules = import.meta.globEager("./modules/**/index.ts");
 
-  let _modules = {};
-  for (const item of Object.keys(storeModules)) {
-    const key = item.match(/[^./]+/g)?.[1] ?? "";
-    const { default: value } = await import(/* @vite-ignore */ item);
-    (_modules as any)[key] = value;
-  }
-  return _modules;
+//   let _modules = {};
+//   for (const item of Object.keys(storeModules)) {
+//     const key = item.match(/[^./]+/g)?.[1] ?? "";
+//     const { default: value } = await import(/* @vite-ignore */ item);
+//     (_modules as any)[key] = value;
+//   }
+//   return _modules;
+// };
+
+// const modules = await asyncStoreModule();
+
+import user from "./modules/user";
+import wechat from "./modules/wechat";
+
+const modules = {
+  user,
+  wechat,
 };
-
-const modules = await asyncStoreModule();
 
 export { modules };
