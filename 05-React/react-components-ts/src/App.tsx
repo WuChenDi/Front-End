6538a1c74468e9dfc73189842a90943be611ac2f
WuChenDi <996194720@qq.com>
2021-09-18 10:15:00 +0800

    feat: add use hooks

--- 05-React/react-components-ts/src/App.tsx
+++ 05-React/react-components-ts/src/App.tsx
@@ -6,49 +6,69 @@ import Hello from './components/Hello'
 import LikeButton from './components/LikeButton'
 import MouseTracker from './components/MouseTracker'
 import useMousePosition from './hooks/useMousePosition'
+import useURLLoader from './hooks/useURLLoader'
+import withLoader from './components/withLoader'
 
-function App() {
-	const [show, setShow] = useState(true)
-	const positions = useMousePosition()
+interface IShowResult {
+  message: string
+  status: string
+}
 
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<Hello />
-				<LikeButton />
+const DogShow: React.FC<{ data: IShowResult }> = ({ data }) => {
+  return (
+    <>
+      <h2>Dog show:{data.status}</h2>
+      <img src={data.message} alt='' />
+    </>
+  )
+}
 
-				<p>
-					<button
-						onClick={() => {
-							setShow(!show)
-						}}
-					>
-						Toggle Tracker
-					</button>
-				</p>
+const App: React.FC = () => {
+  const [show, setShow] = useState(true)
+  // const positions = useMousePosition()
 
-				<>
-					<p>X:{positions.x}</p>
-					<p>Y:{positions.y}</p>
-				</>
+  const WrappedDogShow = withLoader(DogShow, 'https://dog.ceo/api/breeds/image/random')
+  const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show])
 
-				{show && <MouseTracker />}
+  const dogResult = data as IShowResult
 
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	)
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Hello />
+        <LikeButton />
+
+        {loading ? <p>dog loading...</p> : <img src={dogResult && dogResult.message} alt='dog' />}
+
+        <WrappedDogShow />
+
+        <p>
+          <button
+            onClick={() => {
+              setShow(!show)
+            }}
+          >
+            Toggle Tracker
+          </button>
+        </p>
+
+        {/* <>
+          <p>X:{positions.x}</p>
+          <p>Y:{positions.y}</p>
+        </> */}
+
+        {show && <MouseTracker />}
+
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App
