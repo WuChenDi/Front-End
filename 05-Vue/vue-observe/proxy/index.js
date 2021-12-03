1261a5700fba58ac7c1d1185e805af0c945cf6bb
WuChenDi <996194720@qq.com>
2021-12-03 14:14:12 +0800

    feat: 手写 proxy 监听函数

--- 05-Vue/vue-observe/proxy/index.js
+++ 05-Vue/vue-observe/proxy/index.js
@@ -15,6 +15,9 @@ function reactive(target = {}) {
       }
       const result = Reflect.get(target, key, receiver)
       // 深度监听
+      // 性能提升：
+      // Object.defineProperty 是一次性监听完
+      // Proxy 是 get 时候去监听的,仅仅监听当前获取的值的层级,什么时候监听到,什么时候去监听
       return reactive(result)
     },
     set(target, key, val, receiver) {
@@ -22,6 +25,14 @@ function reactive(target = {}) {
       if (val === target[key]) {
         return true
       }
+
+      const ownKeys = Reflect.ownKeys(target)
+      if (ownKeys.includes(key)) {
+        console.log('set 已有 key: ', key)
+      } else {
+        console.log('set 新增 key: ', key)
+      }
+
       const result = Reflect.set(target, key, val, receiver)
       console.log('set', key, val)
       // 是否设置成功
@@ -60,4 +71,10 @@ proxyData.nums.push(6) // 监听数组
 proxyData.xx = 100 // 新增属性
 delete proxyData.nums // 删除属性
 
-// console.log(proxyData)
+// proxyData.a = {}
+// proxyData.a = { b: 200 }
+
+// TODO: 为什么输出没有 a 对象
+// proxyData.a.b = 200
+
+console.log(proxyData)
