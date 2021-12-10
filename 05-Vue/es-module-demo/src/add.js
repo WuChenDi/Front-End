5e32a8e012e6b2c8b8d41a6c402b24a06d845d93
WuChenDi <996194720@qq.com>
2021-12-10 14:33:18 +0800

    feat: 演示 es-module 的使用

---
+++ 05-Vue/es-module-demo/src/add.js
@@ -0,0 +1,6 @@
+import print from './print.js'
+
+export default function add(a, b) {
+  print('print', 'add')
+  return a + b
+}
---
+++ 05-Vue/es-module-demo/src/index.js
@@ -0,0 +1,3 @@
+import { add, multi } from './math.js'
+console.log('add res', add(10, 20))
+console.log('multi res', multi(10, 20))
