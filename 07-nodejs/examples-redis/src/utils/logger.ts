65b4ccf48a872c25216ebabf133811a53e0f4913
wudi <wuchendi96@gmail.com>
2024-04-26 14:39:46 +0800

    refactor: logger.ts to remove unnecessary semicolons

--- 07-nodejs/examples-redis/src/utils/logger.ts
+++ 07-nodejs/examples-redis/src/utils/logger.ts
@@ -1,30 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable no-console */
 
 const getTimestamp = () => {
-  return new Date().toLocaleString() + '.' + new Date().getMilliseconds();
-};
+  return new Date().toLocaleString() + '.' + new Date().getMilliseconds()
+}
 
 const logWithTimestamp = (message?: any, ...optionalParams: any[]) => {
-  console.log(`[${getTimestamp()}]`, message, ...optionalParams);
-};
+  console.log(`[${getTimestamp()}]`, message, ...optionalParams)
+}
 
 const infoWithTimestamp = (message?: any, ...optionalParams: any[]) => {
-  console.info(`[${getTimestamp()}]`, message, ...optionalParams);
-};
+  console.info(`[${getTimestamp()}]`, message, ...optionalParams)
+}
 
 const warnWithTimestamp = (message?: any, ...optionalParams: any[]) => {
-  console.warn(`[${getTimestamp()}]`, message, ...optionalParams);
-};
+  console.warn(`[${getTimestamp()}]`, message, ...optionalParams)
+}
 
 const errorWithTimestamp = (message?: any, ...optionalParams: any[]) => {
-  console.error(`[${getTimestamp()}]`, message, ...optionalParams);
-};
+  console.error(`[${getTimestamp()}]`, message, ...optionalParams)
+}
 
 const logger = {
   log: logWithTimestamp,
   info: infoWithTimestamp,
   warn: warnWithTimestamp,
   error: errorWithTimestamp,
-};
+}
 
-export default logger;
+export default logger
