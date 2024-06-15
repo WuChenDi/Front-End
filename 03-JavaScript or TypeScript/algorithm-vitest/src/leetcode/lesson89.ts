03c48a06fc5a26e9ca9661950e6984e466825b7d
wudi <wuchendi96@gmail.com>
2024-06-15 17:20:43 +0800

    feat(algorithm): add Gray Code Generator with tests

---
+++ 03-JavaScript or TypeScript/algorithm-vitest/src/leetcode/lesson89.ts
@@ -0,0 +1,22 @@
+export function lesson89(n: number): string[] {
+  if (n === 0) {
+    // 基础情况：n为0时直接返回空数组
+    return []
+  }
+  // 递归函数，用来计算输入为n的格雷编码序列
+  const make = (n: number): string[] => {
+    if (n === 1) {
+      return ['0', '1']
+    } else {
+      const prev = make(n - 1)
+      const result: string[] = []
+      const max = 2 ** n - 1
+      for (let i = 0; i < prev.length; i++) {
+        result[i] = `0${prev[i]}`
+        result[max - i] = `1${prev[i]}`
+      }
+      return result
+    }
+  }
+  return make(n)
+}
---
+++ 03-JavaScript or TypeScript/algorithm-vitest/vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import camelCase from 'camelcase'
+import packageJson from './package.json'
+
+const packageName = packageJson.name.split('/').pop() || packageJson.name
+
+export default defineConfig({
+  build: {
+    target: 'ES2018',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs', 'umd', 'iife'],
+      name: camelCase(packageName, { pascalCase: true }),
+      fileName: packageName,
+    },
+  },
+  plugins: [dts({ rollupTypes: true })],
+})
