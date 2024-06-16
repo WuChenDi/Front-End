7924d177ff4c425bbe4fc62d9e3977341c659eee
wudi <wuchendi96@gmail.com>
2024-06-16 09:53:43 +0800

    feat(algorithm-jest): refactor algorithm-jest project to algorithm-vitest project.

--- 03-JavaScript or TypeScript/algorithm-jest/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "extends": ["standard", "plugin:jest/recommended"],
-    rules: {
-        'indent': [4, 4, {
-            'SwitchCase': 2 // 针对switch case的缩进
-        }],
-    }
-};
--- 03-JavaScript or TypeScript/algorithm-jest/test/array/lesson4.test.js
+++
@@ -1,5 +0,0 @@
-import grayCode from "../../code/array/lesson4";
-
-test("grayCode:1", () => {
-  expect(grayCode(2)).toBe(["00", "01", "11", "10"]);
-});
